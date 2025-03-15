cf70cb70ed9e8502aec437c7dc75cfc17f188fa0
milamilstead <mila.milstead@gmail.com>
2025-03-14 21:23:07 -0400

    Complete

--- src/main/java/SkillBuilder4.java
+++ src/main/java/SkillBuilder4.java
@@ -9,10 +9,30 @@
 
 public class SkillBuilder4 {
 
-    // replace this line with your constants
+    public static String findTYPattern(String s) {
+       boolean foundT = false;
+        int TFOUND = -1;
+        int YFOUND = -1;
+        String result = "";
 
-    public static String findTYPattern(String s)
-    {
-        // replace this line with your code
-    }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (!foundT && (ch == 't' || ch == 'T')){
+                foundT = true;
+                TFOUND = i;
+            } else if (foundT && (ch == 'y' || ch == 'Y')) {
+                YFOUND = i;
+                break;
+            }
+            }
+        if ((TFOUND != -1 && YFOUND != -1) && (TFOUND < YFOUND)){
+            result = s.substring(TFOUND,YFOUND+1);
+        }
+
+        return result;
+
 }
+ }
+
+
